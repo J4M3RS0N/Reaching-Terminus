4bf30d76f37c66b9007b3eb57b05aef54518ece9
J4M3RS0N <146147471+J4M3RS0N@users.noreply.github.com>
2024-08-15 20:24:32 +1200

    reverted to old models for screenshots
    also cleaned up code for object grabbing by deleting commented code

--- Assets/Scripts/ItemManagement/ObjectGrabbable.cs
+++ Assets/Scripts/ItemManagement/ObjectGrabbable.cs
@@ -6,7 +6,6 @@ public class ObjectGrabbable : MonoBehaviour
 {
     private Rigidbody objectRb;
     private Transform objectGrabPointTransform;
-    //[SerializeField] private Transform objectGrabPointGO;
     private Collider objectCollider;
 
     [SerializeField] private string interactText;
@@ -15,17 +14,13 @@ public class ObjectGrabbable : MonoBehaviour
     {
         objectRb = GetComponent<Rigidbody>();
         objectCollider = GetComponent<Collider>();
-
     }
     public void Grab(Transform objectGrabPointTransform) 
     {
-        //this.objectGrabPointTransform = objectGrabPointTransform;
-
         this.transform.SetParent(objectGrabPointTransform);
-        //objectCollider.isTrigger = true;
-        //objectCollider.enabled = false;
         objectRb.useGravity = false;
-        //objectRb.isKinematic = true;
+
+        //Constrain Rigibody
         objectRb.constraints = RigidbodyConstraints.FreezePosition;
         objectRb.freezeRotation = true;
     }
@@ -34,10 +29,10 @@ public class ObjectGrabbable : MonoBehaviour
     {
         this.transform.SetParent(null);
         this.objectGrabPointTransform = null;
-        //objectCollider.isTrigger = false;
-        //objectCollider.enabled = true;
+
         objectRb.useGravity = true;
-        //objectRb.isKinematic = false;
+
+        //Unrestrict Rigidbody
         objectRb.constraints = RigidbodyConstraints.None;
         objectRb.freezeRotation = false;
     }
@@ -46,10 +41,6 @@ public class ObjectGrabbable : MonoBehaviour
     {
         if (objectGrabPointTransform != null)
         {
-            //float lerpSpeed = 10f;
-            //Vector3 newPosition = Vector3.Lerp(objectGrabPointTransform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
-            //objectRb.MovePosition(newPosition);
-
             objectRb.transform.position = Vector3.zero;
             objectRb.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
