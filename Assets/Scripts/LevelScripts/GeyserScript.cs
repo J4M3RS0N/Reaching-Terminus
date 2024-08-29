ec2f5db6307d4c6180749d56f7f6aeb913701076
J4M3RS0N <146147471+J4M3RS0N@users.noreply.github.com>
2024-08-29 15:38:51 +1200

    quickly swapped stuff for screenshots on wix

--- Assets/Scripts/LevelScripts/GeyserScript.cs
+++ Assets/Scripts/LevelScripts/GeyserScript.cs
@@ -32,12 +32,7 @@ public class GeyserScript : MonoBehaviour
     {
         fireOffFloat = Random.Range(1.0f, 5.0f);
 
-        // turn on geyser collider + PS and SFX
-
-        //yield return new WaitForSeconds(fireOffFloat);
-
         geyserWarningObj.SetActive(true);
-        //geyserDamageObj.transform.Rotate(0.0f, 0.0f, 0.0f); 
 
         yield return new WaitForSeconds(fireOffFloat);
 
