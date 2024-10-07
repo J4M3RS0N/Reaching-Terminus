be8235224e066c9f4f0824935393cee1431cade6
J4M3RS0N <146147471+J4M3RS0N@users.noreply.github.com>
2024-10-08 11:47:38 +1300

    fixed mech ll tutorial coming up when colliding with hook points, and finally put coolant button switch inside of mech interior

---
+++ Assets/Scripts/MechScripts/MechToggleCoolant.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MechToggleCoolant : MonoBehaviour
+{
+    public UnityEvent mechButtonPush;
+    [SerializeField] private GameObject activeObj;
+    private bool buttonActive;
+
+    [Header("Audio")]
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip clickSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)) 
+        { 
+            PressButton();
+            ToggleButtonIcon();
+        }
+    }
+
+    private void PressButton()
+    {
+        audioSource.PlayOneShot(clickSFX);
+        mechButtonPush.Invoke();
+    }
+    
+    private void ToggleButtonIcon()
+    {
+        buttonActive = !buttonActive;
+
+        if(buttonActive) 
+        { 
+             activeObj.SetActive(true);
+        }
+        else
+        {
+            activeObj.SetActive(false);
+        }
+    }
+}
