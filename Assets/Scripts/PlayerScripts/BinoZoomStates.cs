b947d65acd5d337b5ac5773357dd3a810e3f3fdb
J4M3RS0N <146147471+J4M3RS0N@users.noreply.github.com>
2024-10-13 13:57:50 +1300

    changed bino's to toggle rather than holding down RMB

--- Assets/Scripts/PlayerScripts/BinoZoomStates.cs
+++ Assets/Scripts/PlayerScripts/BinoZoomStates.cs
@@ -5,11 +5,47 @@ using UnityEngine;
 public class BinoZoomStates : MonoBehaviour
 {
     [SerializeField] private Camera playerCam;
+    [SerializeField] private GameObject binoCanvas;
+    [SerializeField] private GameObject objectCam;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource zoomInAudio;
+    [SerializeField] private AudioSource zoomOutAudio;
+
+
+    private float originalFOV;
+
+    private void Awake()
+    {
+        originalFOV = playerCam.fieldOfView;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        //originalFOV = playerCam.fieldOfView;
     }
 
-    
+    public void BinoZoomIn()
+    {
+        playerCam.fieldOfView = 20;
+        //turn off object cam stuff and enable bino soutline like in halo
+        binoCanvas.SetActive(true);
+        objectCam.SetActive(false);
+        zoomOutAudio.enabled = false;
+        zoomInAudio.enabled = true;
+        //play zoom in sound
+    }
+
+    public void BinoZoomOut()
+    {
+        playerCam.fieldOfView = originalFOV;
+        //re-enable object cam stuff and disable bino outline
+        binoCanvas.SetActive(false);
+        objectCam.SetActive(true);
+        zoomInAudio.enabled = false;
+        zoomOutAudio.enabled = true;
+
+        //play zoom out sound
+    }
+
 }
--- Assets/Scripts/PlayerScripts/PilotBinoculars.cs
+++ Assets/Scripts/PlayerScripts/PilotBinoculars.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,56 +6,45 @@ using UnityEngine.Events;
 
 public class PilotBinoculars : MonoBehaviour
 {
-    [SerializeField] private Camera playerCam;
-    [SerializeField] private GameObject binoCanvas;
-    [SerializeField] private GameObject objectCam;
 
-    [Header("Audio")]
-    [SerializeField] private AudioSource zoomAudio;
-    [SerializeField] private AudioClip zIn;
-    [SerializeField] private AudioClip zOut;
+    public UnityEvent OnZoomIn;
+    public UnityEvent OnZoomOut;
 
-    public UnityEvent binoZoomIn;
-    public UnityEvent binoZoomOut;
-    private float originalFOV;
+    private bool zoom;
 
-    private void Start()
-    {
-        originalFOV = playerCam.fieldOfView;
-    }
+    //[SerializeField] private Animator animator;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            binoZoomIn.Invoke();
+            //binoZoomIn?.Invoke();
+            //change bino animation
+            //OnZoomIn?.Invoke();
+
+            ToggleZoom();
         }
 
-        if (Input.GetMouseButtonUp(1))
+        //if (Input.GetMouseButtonUp(1))
+        //{
+        //    //binoZoomOut?.Invoke();
+        //    //chnage bino animation
+        //    OnZoomOut?.Invoke();
+        //}
+    }
+
+    private void ToggleZoom()
+    {
+        zoom = !zoom;
+
+        if (zoom)
         {
-            binoZoomOut.Invoke();
+            OnZoomIn?.Invoke();
         }
-    }
-
-    public void BinoZoomIn()
-    {
-        playerCam.fieldOfView = 20;
-        //turn off object cam stuff and enable bino soutline like in halo
-        binoCanvas.SetActive(true);
-        objectCam.SetActive(false);
-        zoomAudio.PlayOneShot(zIn);
-        //play zoom in sound
-    }
-
-    public void BinoZoomOut()
-    {
-        playerCam.fieldOfView = originalFOV;
-        //re-enable object cam stuff and disable bino outline
-        binoCanvas.SetActive(false);
-        objectCam.SetActive(true);
-        zoomAudio.PlayOneShot(zOut);
-
-        //play zoom out sound
+        else
+        {
+            OnZoomOut?.Invoke();
+        }
     }
 }
